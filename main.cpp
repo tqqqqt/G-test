0f2d1b34c998b6d7e692e0ca4350f272dcecf14e
tqqqqt <tqqqqt@users.noreply.github.com>
2026-04-07 15:38:52 +0700

    some code style

--- main.cpp
+++ main.cpp
@@ -1,35 +1,33 @@
 #include "statistics.h"
 #include "predict.h"
 
-Statistics Stat;
-
 void ReadFile(char *path){
-    
     FILE *f=fopen(path,"rb");
     if (f==NULL){
         printf("File not found"); exit(1);
     }
     unsigned char ch;
     Predictor *Pred;
+    Statistics Stat;
 
     while(!feof(f)){
-        fscanf_s(f,"%c",&ch);
+        fscanf(f,"%c",&ch);
         Stat.Process(ch);            
     }
     Stat.ShowTotalStatistics();
 
-for(int firstPart=700;firstPart<=2900;firstPart+=200){
-    for (int seconPart=50;seconPart<500;seconPart=seconPart+50){
-        Pred=new Predictor(seconPart,firstPart);
-        fseek(f,0,0);
-        while(!feof(f)){
-            fscanf_s(f,"%c",&ch);            
-            Pred->Process(ch);
-        }        
-        Pred->ShowTotalStatistics();
-        delete Pred;
-    }    
-}
+    for(int firstPart=700;firstPart<=2900;firstPart+=200){
+        for (int seconPart=50;seconPart<500;seconPart=seconPart+50){
+            Pred=new Predictor(seconPart,firstPart);
+            fseek(f,0,0);
+            while(!feof(f)){
+                fscanf(f,"%c",&ch);            
+                Pred->Process(ch);
+            }        
+            Pred->ShowTotalStatistics();
+            delete Pred;
+        }    
+    }
     fclose(f);
 }
 
